068c41fc70d0e64d3420af6c35b23e28828f3b37
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:47:03 -0300

    Initial Commit

---
+++ BuildingCustomTransition/BuildingCustomTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingCustomTransitionApp.swift
+//  BuildingCustomTransition
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BuildingCustomTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
